96860f23e993c07909adb2e4b03f2b81138fa0c2
Кого? Чего? Какое имя? <fable>
2023-05-23 13:09:32 +0500

    .

--- php/textInput.php
+++ php/textInput.php
@@ -6,36 +6,23 @@
     $user = $_SESSION['user']['id'];
 
     for($i=0;;$i++){
-        if($_POST['text_' . $i]){
-            $content .= "<div class=\"textPost\"><p>" . str_replace("\n","</p><p>",$_POST['text_'.$i]) . "</p></div>";
-        }
+        if($_POST['text_' . $i]){ $content .= "<div class=\"textPost\"><p>" . str_replace("\n","</p><p>",$_POST['text_'.$i]) . "</p></div>"; }
         else if($_FILES['img_' . $i]){
             $pathUser = 'uploads/content/' . time() . $_FILES['img_' . $i]['name'];
-            if (!move_uploaded_file($_FILES['img_' . $i]['tmp_name'], '../' . $pathUser)) {
-                $pathUser = NULL;
-            }
+            if (!move_uploaded_file($_FILES['img_' . $i]['tmp_name'], '../' . $pathUser)) { $pathUser = NULL; }
             $content .= "<div class=\"imgPost\"><img src=\"" . $pathUser . "\"></div>";            
         }
         else{ break; }
     }
     
-    if($content != ''){
-        mysqli_query(
-            $dbConnection, 
-            "INSERT INTO Post (content, idUser) 
-                VALUES ('$content','$user')"
-        );
-        
+    if($content != ''){ mysqli_query( $dbConnection, "INSERT INTO Post (content, idUser) VALUES ('$content','$user')");
         $idPost = mysqli_insert_id($dbConnection);
-
 
         $tag = '';
         for($i=0;;$i++){
             if($_POST['select_' . $i . '_tag_0']){
                 for($j=0;;$j++){
-                    if($_POST['select_' . $i . '_tag_' . $j]){
-                        $tag .= $_POST['select_' . $i . '_tag_' . $j] . ' ';
-                    }
+                    if($_POST['select_' . $i . '_tag_' . $j]){ $tag .= $_POST['select_' . $i . '_tag_' . $j] . ' '; }
                     else{ break; }
                 }
             }
@@ -44,15 +31,11 @@
         $arrayTag = explode(' ',$tag);
         $arrayTag = array_unique($arrayTag);
 
-        foreach($arrayTag as $idTag){
-            mysqli_query(
-            $dbConnection, 
-            "INSERT INTO Tag_Post (idPost, idTag) 
-                VALUES ('$idPost','$idTag')"
-            );
+        foreach($arrayTag as $idTag){ mysqli_query( $dbConnection, "INSERT INTO Tag_Post (idPost, idTag) VALUES ('$idPost','$idTag')");
         }
     }
 
     header('Location: ../index.php');
     return;
 ?>
+
