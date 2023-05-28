13580687d157d9cde0ab495fe730949894ef346a
Кого? Чего? Какое имя? <fable>
2023-05-28 21:12:22 +0500

    .

--- html/user.php
+++ html/user.php
@@ -2,8 +2,10 @@
 <div class="user">
     <div class="headerUser">
             <img class="imgAvatarUser" src="<?=$_SESSION['user']['avatar']?>" onerror="this.src='img/TestImgAvatar.png'">
-            <p>Name: <?=$_SESSION['user']['name']?></p>
-            <p>ID: <?=$_SESSION['user']['id']?></p>
+            <div>
+                <p>Name: <?=$_SESSION['user']['name']?></p>
+                <p>ID: <?=$_SESSION['user']['id']?></p>
+            </div>
     </div>
 </div>
 
